769b09f6274c9dae5e0a8f9695000633b37959d5
Georgiy Goloborodov <barstyle@mail.ru>
2022-07-13 11:49:32 +0300

    add model User

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -0,0 +1,72 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastName, String email) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "id = " + id +
+               ", firstname = '" + firstname + '\'' +
+               ", lastName = '" + lastName + '\'' +
+               ", email = '" + email + '\'' +
+               '}';
+    }
+}
